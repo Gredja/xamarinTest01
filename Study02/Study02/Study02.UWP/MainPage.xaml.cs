0759efdd2993bf4609dd86b4bbd610cab42b3106
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2017-11-30 17:07:46 +0300

    no

---
+++ Study02/Study02/Study02.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Study02.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadApplication(new Study02.App());
+        }
+    }
+}
