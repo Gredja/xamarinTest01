36b6291ed3efbb95cce2dc8b4a4b2aeb1013e7f8
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2017-12-06 14:11:13 +0300

    ok

---
+++ CreditApp02/CreditApp02/CreditApp02/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+
+namespace CreditApp02
+{
+    public class Bootstrapper
+    {
+        private static Bootstrapper _instance;
+        private readonly ContainerBuilder _builder;
+        private readonly ILifetimeScope _currentScope;
+
+        private Bootstrapper()
+        {
+            _builder = new ContainerBuilder();
+            _currentScope = _builder.Build();
+        }
+
+        public static Bootstrapper Instance => _instance ?? (_instance = new Bootstrapper());
+
+        public T Resolve<T>()
+        {
+            return _currentScope.Resolve<T>();
+        }
+
+        public void Initialize()
+        {
+            //SingleInstance
+          //  _builder.RegisterType<DatabaseRepository>().As<IDatabaseRepository>().SingleInstance();
+
+            //screen viewmodels
+            // _builder.RegisterType<HomeViewModel>().InstancePerLifetimeScope();
+        }
+    }
+}
